dedea76b45878be50e22be059281a55dc7bcc0e4
jprakke <JoshPrakke@gmail.com>
2018-12-13 00:26:38 -0500

    added api directories

--- web/app.py
+++ web/app.py
@@ -1,22 +1,30 @@
 from flask import Flask, jsonify, render_template
-# import numpy as np
-# import sqlalchemy
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import Session
-# from sqlalchemy import create_engine, func, MetaData
-# from sqlalchemy.pool import StaticPool
-# import datetime as dt
-# from datetime import date
+from config import username, password
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, MetaData
+from sqlalchemy.pool import StaticPool
+import datetime as dt
+from datetime import date
 
-#################################################
-# Database Setup
-#################################################
+# ################################################
+#  Database Setup
+# ################################################
 
-# engine = create_engine("sqlite:///Resources/hawaii.sqlite",
-#     connect_args={'check_same_thread':False},
+# connection_string = (f"{username}:{password}@127.0.0.1:3306/craftbeerDB")
+
+# engine = create_engine(f'mysql://{connection_string}',encoding='utf-8',connect_args={'check_same_thread':False},
 #     poolclass=StaticPool)
 
+# Base= automap_base()
+# Base.prepare(engine, reflect=True)
 
+# Beer = Base.classes.beer
+# Breweries = base.classes.breweries
+
+# session. Session(engine)
 
 #################################################
 # Flask Setup
@@ -61,10 +69,28 @@ def popular_styles():
 
 @app.route("/api")
 def api(): 
-    """ api page"""
-    return render_template("api.html")
+    """ returns api.html and list of api routes """
+    api_routes = [
+        "/api/v1.0/beer",
+        "/api/v1.0/breweries",
+        "/api/v1.0/topStyles"
+    ]
+    return render_template("api.html", api_routes = api_routes)
 
+@app.route("/api/v1.0/beer")
+def beer():
+    """json of all beer in dataset"""
+    return("beer")
 
+@app.route("/api/v1.0/breweries")
+def breweries():
+    """json of all breweries in dataset"""
+    return("breweries")
+
+@app.route("/api/v1.0/topStyles")
+def topStyles():
+    """ json of top 10 beer styles"""
+    return("topStyles")
 
 if __name__ == '__main__':
     app.run(debug=False)
